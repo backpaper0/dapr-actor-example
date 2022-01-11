e4eb186f22a9421b016c0ed3100ece557b96ce05
Uragami Taichi <backpaper0@gmail.com>
2022-01-12 07:46:44 +0900

    pubsub

--- src/main/java/com/example/counter/CounterController.java
+++ src/main/java/com/example/counter/CounterController.java
@@ -2,45 +2,68 @@ package com.example.counter;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.dapr.Topic;
 import io.dapr.actors.ActorId;
 import io.dapr.actors.client.ActorClient;
 import io.dapr.actors.client.ActorProxyBuilder;
+import io.dapr.client.DaprClient;
+import io.dapr.client.domain.CloudEvent;
+import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/counter")
 public class CounterController {
 
+	private static final Logger logger = LoggerFactory.getLogger(CounterController.class);
 	private final ActorProxyBuilder<CounterActor> actorProxyBuilder;
+	private final DaprClient daprClient;
 
-	public CounterController(ActorClient actorClient) {
+	public CounterController(ActorClient actorClient, DaprClient daprClient) {
 		this.actorProxyBuilder = new ActorProxyBuilder<>(
 				"CounterActorImpl",
 				CounterActor.class,
 				actorClient);
+		this.daprClient = daprClient;
 	}
 
-	@GetMapping("/{id}")
+	@GetMapping("/counter/{id}")
 	public Object count(@PathVariable ActorId id) {
 		CounterActor counterActor = actorProxyBuilder.build(id);
 		int count = counterActor.count();
 		return Map.of("count", count);
 	}
 
-	@GetMapping("/{id}/sleep/{sleep}")
+	@GetMapping("/counter/{id}/sleep/{sleep}")
 	public Object countWithSleep(@PathVariable long sleep, @PathVariable ActorId id) {
 		CounterActor counterActor = actorProxyBuilder.build(id);
 		int count = counterActor.countWithSleep(sleep);
 		return Map.of("count", count);
 	}
 
-	@GetMapping("/and-hostname/{id}")
+	@GetMapping("/counter/and-hostname/{id}")
 	public Object countAndHostname(@PathVariable ActorId id) {
 		CounterActor counterActor = actorProxyBuilder.build(id);
 		return counterActor.countAndHostname();
+	}
+
+	@GetMapping("/counter/{id}/pub")
+	public Mono<Void> publish(@PathVariable String id) {
+		return daprClient.publishEvent("pubsub", "count", id);
+	}
+
+	@Topic(pubsubName = "pubsub", name = "count")
+	@PostMapping(path = "/counter/subscribe")
+	public void subscribeCount(@RequestBody CloudEvent cloudEvent) {
+		logger.info("Received message: {}", cloudEvent.getData());
+		ActorId id = new ActorId((String) cloudEvent.getData());
+		CounterActor counterActor = actorProxyBuilder.build(id);
+		counterActor.count();
 	}
 }
